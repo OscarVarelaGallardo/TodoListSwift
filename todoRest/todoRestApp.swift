918c481e01a8b7f3cb20f77a9beee8278944ef8f
Oscar Varela <oscar.varela@tilatina.com>
2025-06-11 16:50:50 -0600

    Initial Commit

---
+++ todoRest/todoRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoRestApp.swift
+//  todoRest
+//
+//  Created by Oscar Varela on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct todoRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
