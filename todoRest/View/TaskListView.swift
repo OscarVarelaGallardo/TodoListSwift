9ede9d0a81a5435a613609e2f06904be9c8b9962
Oscar Varela <oscar.varela@tilatina.com>
2025-06-12 01:29:04 -0600

    first commit

---
+++ todoRest/View/TaskListView.swift
@@ -0,0 +1,57 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var appState: AppState
+    @StateObject private var viewModel = TaskViewModel()
+    @State private var newTaskTitle = ""
+
+    var body: some View {
+        HStack {
+            Button("Salir") {
+                appState.isLoggedIn = false
+            }
+            Spacer()
+        }
+        .padding()
+        VStack {
+           
+            HStack {
+                TextField("Escribe una tarea...", text: $newTaskTitle)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(minWidth: 300)
+                
+                Button("Agregar") {
+                    guard !newTaskTitle.isEmpty else { return }
+                    viewModel.addTask(title: newTaskTitle)
+                    newTaskTitle = ""
+                }
+            }
+           
+
+            List {
+                ForEach(viewModel.tasks) { task in
+                    HStack {
+                        Button(action: {
+                            viewModel.toggleTask(task)
+                        }) {
+                            Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle")
+                        }
+                        .buttonStyle(BorderlessButtonStyle())
+
+                        Text(task.title)
+                            .strikethrough(task.isCompleted)
+                    }
+                }
+                .onDelete(perform: viewModel.deleteTask)
+            }
+            .frame(minWidth: 400, minHeight: 300)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
